f2cfdb7e9a80f6135f17f4548194b3b1b1e7af48
yangyao <873241789@qq.com>
2018-12-13 11:55:50 +0800

    引入 immutable 不可变 函数式变成

--- src/reducers/search.ts
+++ src/reducers/search.ts
@@ -5,37 +5,34 @@
  */
 import { search as types } from '../constants/actionType';
 import objectPath from 'object-path';
+import { Map } from 'Immutable';
 
-const initialState: any = {
-  content: {
+const initialState: any = Map({
+  page: {
     songs: [],
     songCount: 0
   },
   processing: false
-};
+});
 
 export function search(state = initialState, action) {
   switch (action.type) {
     case types.startQuery: {
-      return {
-        ...state,
-        processing: true
-      };
+      return initialState
+        .set('processing', true)
     }
     case types.queryComplete: {
       const {success, payload} = action;
       let result = objectPath.get(payload, 'result');
       let songs = objectPath.get(payload, 'result.songs');
-      return {
-        ...state,
-        processing: false,
-        content: (success && result && songs) && payload.result || state.content
-      };
+
+      let page = (success && result && songs) && result || initialState.get('page');
+      return initialState
+        .set('page', page)
+        .set('processing', false)
     }
     default: {
-      return {
-        ...state
-      };
+      return state;
     }
   }
 }
